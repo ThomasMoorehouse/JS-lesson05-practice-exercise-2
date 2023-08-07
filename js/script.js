a0be601713297ca96a01c1ca9545383823553feb
Thomas Moorehouse <moorehousetreasures@gmail.com>
2023-08-06 21:17:37 -0400

    Changed light and dark theme

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var fans = document.querySelector(".fans");
-console.log(fans);
+//console.log(fans);
 var fish = document.querySelector(".fish");
-console.log(fish);
+//console.log(fish);
 var pets = document.querySelector(".pets");
-console.log(pets);
+//console.log(pets);
 
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123K"
@@ -20,3 +20,19 @@ pets.addEventListener("mouseover", function () {
     fish.innerText = "47K"
     pets.innerText = "20K"
 });
+
+var toggle = document.querySelector("#toggle")
+var ball = document.querySelector(".ball")
+var h1 = document.querySelector("h1")
+var body = document.querySelector("body")
+//console.log(body);
+toggle.addEventListener("click", function() {
+    if (body.classList.contains("light")) {
+        body.classList.remove("light")
+        ball.classList.remove("move-right")
+        h1.innerText = "Stealth Quincy"
+    } else {
+        body.classList.add("light")
+        ball.classList.add("move-right")
+    }
+})
